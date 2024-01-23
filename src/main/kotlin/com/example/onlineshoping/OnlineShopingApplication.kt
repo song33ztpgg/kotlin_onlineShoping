0d5c313b3139880476d1a748f068b4d419b4f0a9
sparta <sasasa7737@gmail.com>
2024-01-23 10:59:46 +0900

    첫번재

---
+++ src/main/kotlin/com/example/onlineshoping/OnlineShopingApplication.kt
@@ -0,0 +1,13 @@
+package com.example.onlineshoping
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class OnlineShopingApplication
+
+fun main(args: Array<String>) {
+    print("ch")
+
+    runApplication<OnlineShopingApplication>(*args)
+}
